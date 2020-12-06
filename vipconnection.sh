f3bc804617060476b5a2216439172c7f8f9aafb6
pcfreak <68300186+pcfreak@users.noreply.github.com>
2020-12-06 18:52:52 +0200

    Update vipconnection.sh

--- vipconnection.sh
+++ vipconnection.sh
@@ -2,10 +2,10 @@
 error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', '1');
 
-$DB_host = 'dbsoli.topspeedvpnserver.xyz';
-$DB_user = 'fuzevpnt_solid';
-$DB_pass = 'solidvpn07';
-$DB_name = 'fuzevpnt_solid';
+$DB_host = '162.246.16.67';
+$DB_user = 'wagopan_wago';
+$DB_pass = 'wagopanel2020';
+$DB_name = 'wagopan_wago';
 
 $mysqli = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
 if ($mysqli->connect_error) {
