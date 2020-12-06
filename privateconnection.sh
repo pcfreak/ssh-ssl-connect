81779ad2ec6affae8f78f537a1d81ddd7c3a93a8
pcfreak <68300186+pcfreak@users.noreply.github.com>
2020-12-06 18:54:06 +0200

    Update privateconnection.sh

--- privateconnection.sh
+++ privateconnection.sh
@@ -2,10 +2,10 @@
 error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', '1');
 
-$DB_host = '64.20.63.202';
-$DB_user = 'ehisshco_regina';
-$DB_pass = 'Regina143';
-$DB_name = 'ehisshco_regina';
+$DB_host = '162.246.16.67';
+$DB_user = 'wagopan_wago';
+$DB_pass = 'wagopanel2020';
+$DB_name = 'wagopan_wago';
 
 $mysqli = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
 if ($mysqli->connect_error) {
