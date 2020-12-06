f8fa76bf63ace8a709d22ded6fca731132696fac
pcfreak <68300186+pcfreak@users.noreply.github.com>
2020-12-06 18:51:46 +0200

    Update premiumconnection.sh

--- premiumconnection.sh
+++ premiumconnection.sh
@@ -3,7 +3,7 @@ error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', '1');
 
 $DB_host = '162.246.16.67';
-$DB_user = 'wagopan_qago';
+$DB_user = 'wagopan_wago';
 $DB_pass = 'wagopanel2020';
 $DB_name = 'wagopan_wago';
 
