700dd54a33657b5af44f4e412a0c3c04c2740b11
pcfreak <68300186+pcfreak@users.noreply.github.com>
2020-12-06 10:48:33 +0200

    Update privateconnectin.sh

--- privateconnectin.sh
+++ privateconnectin.sh
@@ -2,10 +2,10 @@
 error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', '1');
 
-$DB_host = '185.61.137.168';
-$DB_user = 'tymlexvp_regina';
-$DB_pass = 'Regina143';
-$DB_name = 'tymlexvp_regina';
+$DB_host = '162.246.16.67';
+$DB_user = 'wagopan_wago';
+$DB_pass = 'wagopanel2020';
+$DB_name = 'wagopan_wago';
 
 $mysqli = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
 if ($mysqli->connect_error) {
